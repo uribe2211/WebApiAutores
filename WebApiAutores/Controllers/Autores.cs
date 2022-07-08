5621cf1a3356e0075f867624b4cba8e29361c5c4
uribe2211 <uribe2211@hotmail.com>
2022-07-08 18:17:40 -0500

    Agregar archivos de proyecto.

---
+++ WebApiAutores/Controllers/Autores.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApiAutores.Entidades;
+
+namespace WebApiAutores.Controllers
+{
+    [ApiController]
+    [Route("api/autores")]
+    public class Autores : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<List<Autor>> Get()
+        {
+            return new List<Autor>()
+            {
+                new Autor(){Id = 1,Nombre = "Felipe"},
+                new Autor(){Id = 2,Nombre = "Anita"}
+            };
+        }
+    }
+}
